2fa14c6f074a768d7346efa3b7fafc2c02dc2fe9
Michael Soohoo <soohoo.mt@gmail.com>
2020-09-14 16:14:40 -0400

    hosting a site

--- ROYGBIV/js/third_party/three_js_roygbiv/three.js/src/renderers/shaders/ShaderChunk/displacementmap_vertex.glsl
+++
@@ -1,6 +0,0 @@
-#ifdef USE_DISPLACEMENTMAP
-
-	vec2 transformedUV = ( uvTransform * vec3( uv, 1 ) ).xy;
-	transformed += normalize( objectNormal ) * ( texture2D( displacementMap, transformedUV ).x * displacementScale + displacementBias );
-
-#endif
