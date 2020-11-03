b409179eed630149f3921c085cc70b0f687cf0da
jamesedmichaud <jamesedmichaud@gmail.com>
2020-11-02 20:51:18 -0500

    Assignment 6 complete

--- 06/robot.js
+++ 06/robot.js
@@ -1,7 +1,11 @@
 function Robot(x, y, z, size) {
     // The human body is approximately 7.5-8 heads in height.
+    this.size = size;
     this.headSize = size/8.0;
     this.joints = {};
+    this.xOffset = x;
+    this.yOffset = y;
+    this.zOffset = z;
 
     // Main trunk
     this.joints.pelvis      = new THREE.Bone();
@@ -140,14 +144,25 @@ function Robot(x, y, z, size) {
 
     // Some prototypes for body movements
     Robot.prototype.moveLeg = function(side, amountAbduct, amountFlex, t) {
+        // If the side isn't left or right, return right away and do nothing
         if (!(side === 'left' || side === 'right')) return;
+
+        // Get the correct joint
         let joint = side === 'left' ? this.joints.lHip : this.joints.rHip;
+
+        // Force the angle to within range of motion
         let angleAbduct = amountAbduct < this.joints.lHip.minAbduct ? this.joints.lHip.minAbduct :
                           amountAbduct > this.joints.lHip.maxAbduct ? this.joints.lHip.maxAbduct : amountAbduct;
         let angleFlex   = amountFlex   < this.joints.lHip.minFlex   ? this.joints.lHip.minFlex   :
                           amountFlex   > this.joints.lHip.maxFlex   ? this.joints.lHip.maxFlex   : amountFlex;
+
+        // Adjust for left vs right side
         angleAbduct = side === 'left' ?  angleAbduct : -angleAbduct;
+
+        // Rotating around x-axis is the same for both sides. Set to negative for more intuitive use.
         angleFlex   = -angleFlex;
+
+        // Slerp calls moved into separate functions for reuse and space-savings.
         slerpXZ(joint, angleFlex, angleAbduct, t);
     }
 
@@ -164,7 +179,7 @@ function Robot(x, y, z, size) {
         if (!(side === 'left' || side === 'right')) return;
         let joint = side === 'left' ? this.joints.lAnkle : this.joints.rAnkle;
         let angle = amount < this.joints.lAnkle.minFlex ? this.joints.lAnkle.minFlex :
-            amount > this.joints.lAnkle.maxFlex ? this.joints.lAnkle.maxFlex : amount;
+                    amount > this.joints.lAnkle.maxFlex ? this.joints.lAnkle.maxFlex : amount;
         angle = side === 'left' ? -angle : angle;
         slerpX(joint, angle, t);
     }
@@ -173,8 +188,7 @@ function Robot(x, y, z, size) {
         if (!(side === 'left' || side === 'right')) return;
         let joint = side === 'left' ? this.joints.lKnee : this.joints.rKnee;
         let angle = amount < this.joints.lKnee.minFlex ? this.joints.lKnee.minFlex :
-            amount > this.joints.lKnee.maxFlex ? this.joints.lKnee.maxFlex : amount;
-        angle = side === 'left' ? -angle : angle;
+                    amount > this.joints.lKnee.maxFlex ? this.joints.lKnee.maxFlex : amount;
         slerpX(joint, angle, t);
     }
 
@@ -212,7 +226,6 @@ function Robot(x, y, z, size) {
         let joint = side === 'left' ? this.joints.lBicep : this.joints.rBicep;
         let angle = amount < this.joints.lBicep.minRotate ? this.joints.lBicep.minRotate :
                     amount > this.joints.lBicep.maxRotate ? this.joints.lBicep.maxRotate : amount;
-        // angle = side === 'left' ? angle : -angle;
         slerpX(joint, angle, t);
     }
 
@@ -220,11 +233,12 @@ function Robot(x, y, z, size) {
         if (!(side === 'left' || side === 'right')) return;
         let joint = side === 'left' ? this.joints.lWrist : this.joints.rWrist;
         let angle = amount < this.joints.lWrist.minFlex ? this.joints.lWrist.minFlex :
-            amount > this.joints.lWrist.maxFlex ? this.joints.lWrist.maxFlex : amount;
+                    amount > this.joints.lWrist.maxFlex ? this.joints.lWrist.maxFlex : amount;
         angle = side === 'left' ?  angle : -angle;
         slerpZ(joint, angle, t);
     }
 
+    // This is for side to side flexing of the wrist
     Robot.prototype.tiltHand = function(side, amount, t) {
         if (!(side === 'left' || side === 'right')) return;
         let joint = side === 'left' ? this.joints.lWrist : this.joints.rWrist;
@@ -234,6 +248,7 @@ function Robot(x, y, z, size) {
         slerpY(joint, angle, t);
     }
 
+    // Used to tilt the pelvis for kick motion
     Robot.prototype.tiltBody = function(direction, amount, t) {
         let joint = this.joints.pelvis;
         if (direction === 'z') {
@@ -251,6 +266,7 @@ function Robot(x, y, z, size) {
     Robot.prototype.tiltBack = function(amount, t) {
         let lowBack = this.joints.lowerBack;
         let midBack = this.joints.midBack;
+        // Split the flex between two back joints
         let splitLow = amount * 5.0/7.0;
         let splitMid = amount * 2.0/7.0;
         let angleLow = splitLow < lowBack.minFlex ? lowBack.minFlex :
@@ -309,7 +325,7 @@ function Robot(x, y, z, size) {
         ).normalize(), t);
     }
 
-    // Set a starting position for the robot. Right hand on hip. Left hand near chin
+    // Set a starting position for the robot. Arms loose at sides
     this.moveShoulder('left', 0, -90, 1);
     this.moveShoulder('right', 0, -90, 1);
     this.flexElbow('left', 20, 1);
@@ -319,12 +335,90 @@ function Robot(x, y, z, size) {
     this.flexWrist('left',  20, 1);
     this.flexWrist('right',  10, 1);
 
-
     // Save all quaternions as starting positions
     for (let joint in this.joints) {
         this.joints[joint].startingPosition = this.joints[joint].quaternion.clone();
     }
 
+    // Let's make an array of dance moves.
+    this.danceMoves = [];
+
+    this.danceMoves.push((speed) => {
+        let side = Math.random() < 0.5 ? 'left' : 'right';
+        let joint = this.joints.lHip;
+        let angleFlex   = Math.random() * (joint.maxFlex   - joint.minFlex  ) + joint.minFlex;
+        let angleAbduct = Math.random() * (joint.maxAbduct - joint.minAbduct) + joint.minAbduct;
+        this.moveLeg(side, angleFlex, angleAbduct, speed);
+    });
+    this.danceMoves.push((speed) => {
+        let side = Math.random() < 0.5 ? 'left' : 'right';
+        let joint = this.joints.lHipRotator;
+        let angle = Math.random() * (joint.maxRotate - joint.minRotate) + joint.minRotate;
+        this.rotateHip(side, angle, speed);
+    });
+    this.danceMoves.push((speed) => {
+        let side = Math.random() < 0.5 ? 'left' : 'right';
+        let joint = this.joints.lAnkle;
+        let angle = Math.random() * (joint.maxFlex - joint.minFlex) + joint.minFlex;
+        this.flexAnkle(side, angle, speed);
+    });
+    this.danceMoves.push((speed) => {
+        let side = Math.random() < 0.5 ? 'left' : 'right';
+        let joint = this.joints.lKnee;
+        let angle = Math.random() * (joint.maxFlex - joint.minFlex) + joint.minFlex;
+        this.flexKnee(side, angle, speed);
+    });
+    this.danceMoves.push((speed) => {
+        let side = Math.random() < 0.5 ? 'left' : 'right';
+        let joint = this.joints.lElbow;
+        let angle = Math.random() * (joint.maxFlex - joint.minFlex) + joint.minFlex;
+        this.flexElbow(side, angle, speed);
+    });
+    this.danceMoves.push((speed) => {
+        let side = Math.random() < 0.5 ? 'left' : 'right';
+        let joint = this.joints.lForearm;
+        let angle = Math.random() * (joint.maxSpin - joint.minSpin) + joint.minSpin;
+        this.spinForearm(side, angle, speed);
+    });
+    this.danceMoves.push((speed) => {
+        let side = Math.random() < 0.5 ? 'left' : 'right';
+        let joint = this.joints.lShoulder;
+        let angleSwing = Math.random() * (joint.maxSwing - joint.minSwing) + joint.minSwing;
+        let angleRaise = Math.random() * (joint.maxRaise - joint.minRaise) + joint.minRaise;
+        this.moveShoulder(side, angleSwing, angleRaise, speed);
+    });
+    this.danceMoves.push((speed) => {
+        let side = Math.random() < 0.5 ? 'left' : 'right';
+        let joint = this.joints.lBicep;
+        let angle = Math.random() * (joint.maxRotate - joint.minRotate) + joint.minRotate;
+        this.rotateShoulder(side, angle, speed);
+    });
+    this.danceMoves.push((speed) => {
+        let side = Math.random() < 0.5 ? 'left' : 'right';
+        let joint = this.joints.lWrist;
+        let angle = Math.random() * (joint.maxFlex - joint.minFlex) + joint.minFlex;
+        this.flexWrist(side, angle, speed);
+    });
+    this.danceMoves.push((speed) => {
+        let side = Math.random() < 0.5 ? 'left' : 'right';
+        let joint = this.joints.lHand;
+        let angle = Math.random() * (joint.maxFlex - joint.minFlex) + joint.minFlex;
+        this.tiltHand(side, angle, speed);
+    });
+    this.danceMoves.push((speed) => {
+        let joint = this.joints.upperBack;
+        let angle = Math.random() * (joint.maxRotate - joint.minRotate) + joint.minRotate;
+        this.rotateBack(angle, speed);
+    });
+    this.danceMoves.push((speed) => {
+        let lowBack = this.joints.lowerBack;
+        let midBack = this.joints.midBack;
+        let magnitude = (lowBack.maxFlex+midBack.maxFlex - lowBack.minFlex-midBack.minFlex);
+        let angle = Math.random() * magnitude  + lowBack.minFlex+midBack.minFlex;
+        this.tiltBack(angle, speed);
+    });
+
+    // And some pre-set pose functions
     Robot.prototype.setRaiseLeftArmPose = function(t) {
         this.moveShoulder('left', 0, 90, t);
         this.rotateShoulder('left', -90, t);
@@ -341,33 +435,14 @@ function Robot(x, y, z, size) {
         this.flexWrist('left', 0, t);
     }
 
-    Robot.prototype.setKickPose = function(t) {
-        this.rotateHip('left', 40, t);
-        this.rotateHip('right', 40, t);
-        this.tiltBody('z', -60, t);
-        this.tiltBack(35, t);
-        this.rotateBack(-35, t);
-        this.moveLeg('left', 60,0, t);
-        this.moveLeg('right', 60,0, t);
-        this.moveShoulder('left', 60, -40, t);
-        this.moveShoulder('right', -30, -90, t);
-        this.rotateShoulder('left', 10, t);
-        this.rotateShoulder('right', -20, t);
-        this.flexElbow('left', 130, t);
-        this.flexElbow('right', 110, t);
-        this.spinForearm('left',  60, t);
-        this.spinForearm('right', 60, t);
-        this.flexWrist('left', 20, t);
-        this.flexWrist('right', 0, t);
-    }
-
     Robot.prototype.setWavePose = function(t) {
         let minElbow = 30;
         let maxElbow = 60;
         let maxHand =  20;
         let minHand = -10;
-        let elbowFlex = Math.sin(this.waveTimer)*(maxElbow-minElbow) + minElbow;
-        let handTilt = -Math.sin(this.waveTimer)*(maxHand-minHand) + minHand;
+        // periodic function allows repeated hand waving
+        let elbowFlex = Math.sin(this.waveTimer) * (maxElbow-minElbow) + minElbow;
+        let handTilt = -Math.sin(this.waveTimer) * (maxHand-minHand) + minHand;
         this.moveShoulder('right', 0, 50, t);
         this.rotateShoulder('right', -90, t);
         this.spinForearm('right', -40, t);
@@ -390,6 +465,64 @@ function Robot(x, y, z, size) {
         this.flexWrist('right', -50, t);
     }
 
+    // Step one is to get on one leg, raised leg bent (ready to kick out)
+    Robot.prototype.setKickPoseStepOne = function(t) {
+        this.rotateHip('left', 40, t);
+        this.rotateHip('right', 50, t);
+        this.tiltBody('z', -60, t);
+        this.tiltBack(35, t);
+        this.rotateBack(-35, t);
+        this.moveLeg('left', 60,0, t);
+        this.moveLeg('right', 60,120, t);
+        this.flexKnee('right', 120, t)
+        this.moveShoulder('left', 60, -40, t);
+        this.moveShoulder('right', -30, -90, t);
+        this.rotateShoulder('left', 10, t);
+        this.rotateShoulder('right', -20, t);
+        this.flexElbow('left', 130, t);
+        this.flexElbow('right', 110, t);
+        this.spinForearm('left',  60, t);
+        this.spinForearm('right', 60, t);
+        this.flexWrist('left', 20, t);
+        this.flexWrist('right', 0, t);
+    }
+
+    // Step two quickly extends the foot by un-bending knee and hip
+    Robot.prototype.setKickPoseStepTwo = function(t) {
+        this.rotateHip('left', 40, t);
+        this.rotateHip('right', 40, t);
+        this.tiltBody('z', -60, t);
+        this.tiltBack(35, t);
+        this.rotateBack(-35, t);
+        this.moveLeg('left', 60, 0, t);
+        this.moveLeg('right', 60, 0, t);
+        this.flexKnee('right', -10, t)
+        this.moveShoulder('left', 60, -40, t);
+        this.moveShoulder('right', -30, -90, t);
+        this.rotateShoulder('left', 10, t);
+        this.rotateShoulder('right', -20, t);
+        this.flexElbow('left', 130, t);
+        this.flexElbow('right', 110, t);
+        this.spinForearm('left', 60, t);
+        this.spinForearm('right', 60, t);
+        this.flexWrist('left', 20, t);
+        this.flexWrist('right', 0, t);
+    }
+
+    Robot.prototype.setKickPose = function(t) {
+        // This waits for the lower back to get in position before step two
+        if (this.joints.lowerBack.quaternion.z < 0.21) {
+            this.setKickPoseStepOne(t);
+        } else {
+            // 4x as fast, for snappy kick action
+            this.setKickPoseStepTwo(4*t <= 1 ? 4*t : 1);
+            // kickTimer allows the robot to hold the pose for about a second after knee is extended
+            if (this.joints.rKnee.quaternion.x < 0.04 && this.kickTimer++ > 60) {
+                this.movement = 'startingPose';
+            }
+        }
+    }
+
     Robot.prototype.setResetPose = function(t) {
         for (let joint in this.joints) {
             this.joints[joint].quaternion.slerp(new THREE.Quaternion(0, 0, 0, 1), t);
@@ -402,33 +535,44 @@ function Robot(x, y, z, size) {
         }
     }
 
+    Robot.prototype.setDance = function() {
+        let move = Math.floor(Math.random() * this.danceMoves.length);
+        let speed = Math.random() * (0.2 - 0.01) + 0.2
+        this.danceMoves[move](speed);
+    }
+
     Robot.prototype.show = function (scene) {
         const rGroup = new THREE.Group();
         rGroup.add(this.joints.pelvis);
         const helper = new THREE.SkeletonHelper(rGroup);
-        helper.material.linewidth = 1; // make the skeleton thick (doesn't work on my machine)
+        helper.material.linewidth = 3; // make the skeleton thick (doesn't work on my machine)
         scene.add(rGroup);
         scene.add(helper);
     }
 
     Robot.prototype.raise_left_arm  = function() { this.movement = 'raise_left_arm'; }
     Robot.prototype.lower_left_arm  = function() { this.movement = 'lower_left_arm'; }
-    Robot.prototype.kick            = function() { this.movement = 'kick';           }
     Robot.prototype.resetPose       = function() { this.movement = 'resetPose';      }
     Robot.prototype.startingPose    = function() { this.movement = 'startingPose';   }
     Robot.prototype.crossArms       = function() { this.movement = 'crossArms';      }
-    Robot.prototype.wave            = function() {
+    Robot.prototype.dance           = function() { this.movement = 'dance';          }
+    Robot.prototype.wave = function() {
         this.waveTimer = 0;
         this.movement = 'wave';
+    }
+    Robot.prototype.kick = function() {
+        this.kickTimer = 0;
+        this.movement = 'kick';
     }
 
     Robot.prototype.onAnimate = function() {
         if (this.movement === 'raise_left_arm')         this.setRaiseLeftArmPose(0.05);
         else if (this.movement === 'lower_left_arm')    this.setLowerLeftArmPose(0.05);
-        else if (this.movement === 'kick')              this.setKickPose(0.05);
         else if (this.movement === 'startingPose')      this.setStartingPose(0.05);
         else if (this.movement === 'resetPose')         this.setResetPose(0.1);
         else if (this.movement === 'wave')              this.setWavePose(0.1);
-        else if (this.movement === 'crossArms')         this.setArmsCrossPose(0.1);
+        else if (this.movement === 'crossArms')         this.setArmsCrossPose(0.05);
+        else if (this.movement === 'kick')              this.setKickPose(0.05);
+        else if (this.movement === 'dance')             this.setDance();
     }
 }
--- index.md
+++ index.md
@@ -8,3 +8,4 @@ Assignment 2: [pdf](https://jamesedmichaud.github.io/02/Michaud_CS460_Assignment
 Assignment 3: [pdf](https://jamesedmichaud.github.io/03/Michaud_CS460_Assignment_03.pdf) and [animation](https://jamesedmichaud.github.io/03/index.html)  
 Assignment 4: [pdf](https://jamesedmichaud.github.io/04/Michaud_CS460_Assignment_04.pdf) and [animation](https://jamesedmichaud.github.io/04/index.html)  
 Assignment 5: [pdf](https://jamesedmichaud.github.io/05/Michaud_CS460_Assignment_05-2.pdf) and [animation](https://jamesedmichaud.github.io/05/index.html)  
+Assignment 6: [pdf](https://jamesedmichaud.github.io/06/Michaud_CS460_Assignment_06.pdf) and [animation](https://jamesedmichaud.github.io/06/index.html)  
