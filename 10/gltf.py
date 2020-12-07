6f35cf27c652f6167c8d6c312c36df70ee5b8719
bobbyxing <bobbyxing1997@gmail.com>
2020-12-07 18:05:34 -0500

    Hw10

--- 10/gltf.py
+++ 10/gltf.py
@@ -1,18 +1,61 @@
 import numpy as np
 import base64
 
-VERTICES = np.array([0.,0.,0.,    0.,1.,0.,    1.,0.,0.], dtype=np.float32)
-INDICES = np.array([0, 1, 2], dtype=np.ushort)
+VERTICES_IN_FILE = []
+INDICES_IN_FILE = []
+fileObj = open("cube.obj", "r")
 
-HOWMANY = 3
-MAX_X = 1
-MAX_Y = 1
-MAX_Z = 0
-MIN_X = 0
-MIN_Y = 0
-MIN_Z = 0
-MAX = 2
-MIN = 0
+for line in fileObj.readlines():
+    list = line.split()
+    if list[0] == 'v':
+        VERTICES_IN_FILE.append(float(list[1]))
+        VERTICES_IN_FILE.append(float(list[2]))
+        VERTICES_IN_FILE.append(float(list[3]))
+    if list[0] == 'f':
+        INDICES_IN_FILE.append(float(list[1]) - 1)
+        INDICES_IN_FILE.append(float(list[2]) - 1)
+        INDICES_IN_FILE.append(float(list[3]) - 1)
+
+fileObj.close()
+
+VERTICES = np.array(VERTICES_IN_FILE, dtype=np.float32)
+INDICES = np.array(INDICES_IN_FILE, dtype=np.int16)
+
+HOWMNAY_V = len(VERTICES_IN_FILE) // 3
+HOWMANY_I = len(INDICES_IN_FILE)
+
+MAX_X = -10000
+MAX_Y = -10000
+MAX_Z = -10000
+MIN_X = 10000
+MIN_Y = 10000
+MIN_Z = 10000
+MAX = -10000
+MIN = 10000
+
+indexX = 0
+while indexX < len(VERTICES_IN_FILE):
+    MAX_X = max(VERTICES_IN_FILE[indexX], MAX_X)
+    MIN_X = min(VERTICES_IN_FILE[indexX], MIN_X)
+    indexX += 3
+
+indexY = 1
+while indexY < len(VERTICES_IN_FILE):
+    MAX_Y = max(VERTICES_IN_FILE[indexY], MAX_Y)
+    MIN_Y = min(VERTICES_IN_FILE[indexY], MIN_Y)
+    indexY += 3
+
+indexZ = 2
+while indexZ < len(VERTICES_IN_FILE):
+    MAX_Z = max(VERTICES_IN_FILE[indexZ], MAX_Z)
+    MIN_Z = min(VERTICES_IN_FILE[indexZ], MIN_Z)
+    indexZ += 3
+
+index = 0
+while index < len(INDICES_IN_FILE):
+    MAX = max(INDICES_IN_FILE[index], MAX)
+    MIN = min(INDICES_IN_FILE[index], MIN)
+    index += 1
 
 HOWMANYBYTES_V = VERTICES.nbytes
 HOWMANYBYTES_I = INDICES.nbytes
@@ -26,12 +69,12 @@ gltf = {
         "generator": "CS460 Magic Fingers"
     },
 
-  "accessors": [
+    "accessors": [
         {
             "bufferView": 0,
             "byteOffset": 0,
             "componentType": 5126,
-            "count": HOWMANY,
+            "count": HOWMNAY_V,
             "type": "VEC3",
             "max": [MAX_X, MAX_Y, MAX_Z],
             "min": [MIN_X, MIN_Y, MIN_Z]
@@ -40,12 +83,12 @@ gltf = {
             "bufferView": 1,
             "byteOffset": 0,
             "componentType": 5123,
-            "count": HOWMANY,
+            "count": HOWMANY_I,
             "type": "SCALAR",
             "max": [MAX],
             "min": [MIN]
         }
-    ], 
+    ],
 
     "bufferViews": [
         {
@@ -61,26 +104,36 @@ gltf = {
             "target": 34963
         }
     ],
-    
+
     "buffers": [
         {
-            "uri": "data:application/octet-stream;base64,"+str(B64_VERTICES, 'utf-8'),
+            "uri": "data:application/octet-stream;base64," + str(B64_VERTICES, 'utf-8'),
             "byteLength": HOWMANYBYTES_V
         },
         {
-            "uri": "data:application/octet-stream;base64,"+str(B64_INDICES, 'utf-8'),
+            "uri": "data:application/octet-stream;base64," + str(B64_INDICES, 'utf-8'),
             "byteLength": HOWMANYBYTES_I
         }
     ],
-  
+
+    "materials": [
+         {
+            "pbrMetallicRoughness": {
+            "baseColorFactor":
+                [1.0, 0, 1, 1]
+            }
+        }
+      ],
+
     "meshes": [
         {
             "primitives": [{
-                 "mode": 4,
-                 "attributes": {
-                     "POSITION": 0
-                 },
-                 "indices": 1
+                "mode": 4,
+                "material": 0,
+                "attributes": {
+                    "POSITION": 0
+                },
+            "indices": 1,
             }]
         }
     ],
@@ -102,5 +155,5 @@ gltf = {
     "scene": 0
 }
 
-print ( str(gltf).replace("'", '"') ) # we need double quotes instead of single quotes
+print(str(gltf).replace("'", '"'))  # we need double quotes instead of single quotes
 
