3a317dcaf383b0c366e7ea09990ad2b8168b9f4a
William Hem <William.Hem001@umb.edu>
2020-12-07 16:47:20 -0500

    Assignment 10!

--- 10/gltf.py
+++ 10/gltf.py
@@ -1,21 +1,52 @@
 import numpy as np
 import base64
 
-VERTICES = np.array([0.,0.,0.,    0.,1.,0.,    1.,0.,0.], dtype=np.float32)
-INDICES = np.array([0, 1, 2], dtype=np.ushort)
+# Airplane Geometry obtained here: https://people.sc.fsu.edu/~jburkardt/data/ply/ply.html
 
-HOWMANY = 3
-MAX_X = 1
-MAX_Y = 1
-MAX_Z = 0
-MIN_X = 0
-MIN_Y = 0
-MIN_Z = 0
-MAX = 2
+VERTICES = []
+INDICES = []
+
+MAX_X = np.NInf
+MAX_Y = np.NInf
+MAX_Z = np.NInf
+MIN_X = np.Inf
+MIN_Y = np.Inf
+MIN_Z = np.Inf
+MAX = np.NInf
 MIN = 0
+#open the file
+file = open("airplane.ply", "r")
+counter = 1
+for one_line in file:
+    if (counter <= 9): # Header 12 lines long
+        counter+=1
+        continue
+    values = one_line.split()
+    values_read = []
+    check = 0
+    for val in values:
+        values_read.append(float(val))
+        check+=1
+    if (check == 3):    # Line is a vertex
+        MAX_X = max(MAX_X, values_read[0])
+        MAX_Y = max(MAX_Y, values_read[1])
+        MAX_Z = max(MAX_X, values_read[2])
+        MIN_X = max(MIN_X, values_read[0])
+        MIN_Y = max(MIN_X, values_read[1])
+        MIN_Z = max(MIN_X, values_read[2])
+        VERTICES.append(values_read)
+    else:               # Line is an index
+        MAX = max(MAX, values_read[1], values_read[2], values_read[3])
+        INDICES.append(values_read[1:])
 
+    VERTICES = np.array(VERTICES, dtype=np.float32)
+    INDICES = np.array(INDICES, dtype=np.ushort)
+
+
+HOWMANY_V = len(VERTICES)
+HOWMANY_I = len(INDICES) * 3 # Each vertex is of length 3 this we multiply the indicies length by 3  
 HOWMANYBYTES_V = VERTICES.nbytes
-HOWMANYBYTES_I = INDICES.nbytes
+HOWMANYBYTES_I = INDICES.nbytes 
 
 B64_VERTICES = base64.b64encode(VERTICES)
 B64_INDICES = base64.b64encode(INDICES)
@@ -31,7 +62,7 @@ gltf = {
             "bufferView": 0,
             "byteOffset": 0,
             "componentType": 5126,
-            "count": HOWMANY,
+            "count": HOWMANY_V,
             "type": "VEC3",
             "max": [MAX_X, MAX_Y, MAX_Z],
             "min": [MIN_X, MIN_Y, MIN_Z]
@@ -40,7 +71,7 @@ gltf = {
             "bufferView": 1,
             "byteOffset": 0,
             "componentType": 5123,
-            "count": HOWMANY,
+            "count": HOWMANY_I,
             "type": "SCALAR",
             "max": [MAX],
             "min": [MIN]
