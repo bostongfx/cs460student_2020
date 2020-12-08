ed15d830b0e13a2d47aa56504c1d7131524d7040
Tony Chau <tony.chau002@umb.edu>
2020-12-07 20:23:15 -0500

    Assignment 10 complete

--- 10/gltf.py
+++ 10/gltf.py
@@ -1,18 +1,75 @@
 import numpy as np
 import base64
 
-VERTICES = np.array([0.,0.,0.,    0.,1.,0.,    1.,0.,0.], dtype=np.float32)
-INDICES = np.array([0, 1, 2], dtype=np.ushort)
+ 
 
-HOWMANY = 3
-MAX_X = 1
-MAX_Y = 1
-MAX_Z = 0
-MIN_X = 0
-MIN_Y = 0
-MIN_Z = 0
-MAX = 2
-MIN = 0
+VERTICES = [] #np.array([], dtype=np.float32)
+INDICES = [] #np.array([], dtype=np.ushort)
+
+HOWMANY_V = 0
+HOWMANY_I = 0
+MAX_X = float("-inf")
+MAX_Y = float("-inf")
+MAX_Z = float("-inf")
+MIN_X = float("inf")
+MIN_Y = float("inf")
+MIN_Z = float("inf")
+MAX = float("-inf")
+MIN = float("inf")
+
+
+count = 0
+end_header = False
+numOfVertices = 0
+indicesIndex = 0
+tempArray = []
+
+## Reading file by line.
+fileObj = open("dolphins.ply", "r")
+for line in fileObj.readlines():
+    if line[0:10] == "end_header":
+        end_header = True
+    elif line[0:14] == "element vertex":
+        numOfVertices = int(line[15:]) 
+        numOfVertices = numOfVertices - 1
+        HOWMANY_V = int(line[15:18]) 
+    elif end_header and count <= numOfVertices:
+        for i in line.split(' ')[:3]:
+            tempArray.append(i)
+        VERTICES.append(tempArray)
+        count += 1
+    elif end_header and count > numOfVertices:
+        for i in line.split(' ')[1:4]:
+            tempArray.append(i)
+        INDICES.append(tempArray)
+        HOWMANY_I += 1
+    tempArray = []
+fileObj.close()
+VERTICES = np.array(VERTICES, dtype=np.float32)
+INDICES = np.array(INDICES, dtype=np.ushort)
+
+
+HOWMANY_I = HOWMANY_I *3
+for a in VERTICES:
+    if MAX_X < a[0]:
+        MAX_X = a[0]
+    elif MIN_X > a[0]:
+        MIN_X = a[0]
+    if MAX_Y < a[1]:
+        MAX_Y = a[1]
+    elif MIN_Y > a[1]:
+        MIN_Y = a[1]
+    if MAX_Z < a[2]:
+        MAX_Z = a[2]
+    elif MIN_Z > a[2]:
+         MIN_Z = a[2]
+
+for a in INDICES:
+    for b in a:
+        if MIN > b:
+            MIN = b
+        elif MAX < b:
+            MAX = b
 
 HOWMANYBYTES_V = VERTICES.nbytes
 HOWMANYBYTES_I = INDICES.nbytes
@@ -31,7 +88,7 @@ gltf = {
             "bufferView": 0,
             "byteOffset": 0,
             "componentType": 5126,
-            "count": HOWMANY,
+            "count": HOWMANY_V,
             "type": "VEC3",
             "max": [MAX_X, MAX_Y, MAX_Z],
             "min": [MIN_X, MIN_Y, MIN_Z]
@@ -40,7 +97,7 @@ gltf = {
             "bufferView": 1,
             "byteOffset": 0,
             "componentType": 5123,
-            "count": HOWMANY,
+            "count": HOWMANY_I,
             "type": "SCALAR",
             "max": [MAX],
             "min": [MIN]
@@ -102,5 +159,8 @@ gltf = {
     "scene": 0
 }
 
-print ( str(gltf).replace("'", '"') ) # we need double quotes instead of single quotes
+print ( str(gltf).replace("'", '"') ) 
 
+f = open("output.gltf", "w")
+f.write(str(gltf).replace("'", '"'))
+f.close()
